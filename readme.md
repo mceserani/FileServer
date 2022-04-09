186d4602b86e2055763091ea61f966c96709c2d3
Matteo Ceserani <ceserani@gmail.com>
2022-04-09 09:34:34 +0200

    Added readme.md

---
+++ readme.md
@@ -0,0 +1,20 @@
+- Il client si connette al server e gli invia
+	1. Il nome di un file (ricevuto da riga di comando) esistente sulla macchina client
+	2. Il file stesso
+- Esempio di esecuzione del client
+	java fileClient <serverIP> 10000 <nomeFile>
+- Il server controlla se il file esiste, e se non esiste accetta il trasferimento e salva il file nella directory in cui è in esecuzione
+- Architettura multithread: il server ha un processo principale che accetta le richieste di connessione, quando riceve una richiesta crea un thread che gestisce il trasferimento del file
+- In ascolto sulla porta 10000
+- Esempio di esecuzione del server
+	java fileServer 10000
+	
+	
+	String path = "C:\Users\m_ces\Documents\Video Scuola\5 AIA 2122\Appunti\Sistemi e Reti\40\MyThread.java";
+	File f = new File(path);
+	DataOutputStream fout = new DataOutputStream(new FileOutputStream(path));
+	DataInputStream fin = new DataInputStream(new FileInputStream(path));
+	
+	int b = 0;
+	fout.write(b)
+	b = fin.read();
--- src/main/java/it/edu/polomanettiporciatti/fileserver/FileServerProtocol.java
+++ src/main/java/it/edu/polomanettiporciatti/fileserver/FileServerProtocol.java
@@ -2,6 +2,7 @@ package it.edu.polomanettiporciatti.fileserver;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -20,13 +21,41 @@ public class FileServerProtocol extends Thread{
     @Override
     public void run() {
 
-        // Qui ci va il codice del protocollo
-        // Ricevo il nome di un file dal client
-        // Verifico se il file esiste
-        // Se esiste ripondo "KO" e termino
-        // Se non esiste rispondo "OK" e attendo che il client mi invii il file
-        // Salvo il file nella directory in cui mi trovo
-        // Al termine chiudo la connessione
+        // Receive filename from client
+        String fileName = null;
+        try {
+            fileName = in.readUTF();
+        } catch (IOException e) {
+            // Close the socket and exit
+            try {
+                clientSocket.close();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return;
+        }
+
+        // Check if the file exists
+        File file = new File(fileName);
+        if (file.exists()) {
+            // If exists send error message
+            // and close the socket
+            try {
+                out.writeUTF("KO");
+                clientSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+
+        // Else, if the file does not exist, send OK
+
+        // Receives the file from the client
+        // and write it to the file system
+        
+
+        // Close the socket
     
     }
 
